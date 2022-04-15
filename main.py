44388473ed8b8802da74bb933020e89775382e44
Anton Yusupov <antonyusupov@Antons-MacBook-Air.local>
2022-04-15 09:03:52 +0300

    lovely-anton-bot_2

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import telebot
 from telebot import types
 from random import choice
+from api import Giphy
 
+giphy = Giphy()
 bot = telebot.TeleBot('5374635129:AAG9l18dW7vpsHpIV4V13lW51nBVaIu_JVI')
-kisses = ['https://media.giphy.com/media/lTQF0ODLLjhza/giphy.gif', 'https://media.giphy.com/media/dMYVHzANYb9p6/giphy.gif', 'https://media.giphy.com/media/Vi0Ws3t4JSLOgdkaBq/giphy-downsized-large.gif']
-hugs = ['https://media.giphy.com/media/l8ooOxhcItowwLPuZn/giphy.gif', 'https://media.giphy.com/media/EvYHHSntaIl5m/giphy.gif', 'https://media.giphy.com/media/QbkL9WuorOlgI/giphy.gif']
 sayings = ['I love you like a fat kid loves cake.', 'You want to know who I am in love with? Read the first word again.', 'I love you with all my belly. I would say heart, but my belly is bigger.']
 
 @bot.message_handler(commands=['start'])
@@ -18,25 +18,35 @@ def help(message):
     kiss = types.KeyboardButton('/kiss')
     hug = types.KeyboardButton('/hug')
     saying = types.KeyboardButton('/saying')
-    markup.add(kiss, hug, saying)
+    joke = types.KeyboardButton('/joke')
+    iss_location = types.KeyboardButton('/iss')
+    markup.add(kiss, hug, saying, joke, iss_location)
     bot.send_message(message.chat.id, 'Choose the option', reply_markup=markup)
 
 
 @bot.message_handler(commands=['kiss'])
-def help(message):
-    bot.send_animation(message.chat.id, choice(kisses))
+def kiss(message):
+    bot.send_animation(message.chat.id, giphy.get_url("kiss"))
 
 @bot.message_handler(commands=['hug'])
-def help(message):
-    bot.send_animation(message.chat.id, choice(hugs))
+def hug(message):
+    bot.send_animation(message.chat.id, giphy.get_url("hug"))
 
 @bot.message_handler(commands=['saying'])
-def help(message):
+def saying(message):
     bot.send_message(message.chat.id, choice(sayings))
 
-@bot.message_handler()
-def help(message):
-    bot.send_message(message.chat.id, "I don't get it")
+@bot.message_handler(commands=['joke'])
+def joke(message):
+    bot.send_message(message.chat.id, giphy.get_joke()[0])
+    bot.send_message(message.chat.id, giphy.get_joke()[1])
 
+@bot.message_handler(commands=['iss'])
+def iss(message):
+    bot.send_location(message.chat.id, giphy.location()[0], giphy.location()[1])
+
+@bot.message_handler()
+def message(message):
+    bot.send_message(message.chat.id, "I don't get it")
 
 bot.polling(non_stop=True)
